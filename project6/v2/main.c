947aa50a9d05494157a4acfb3a5878e6c8f8a712
Jonas Tiskus <jonas.tiskus4@gmail.com>
2026-03-06 00:24:29 +0200

    Implement parser partially

--- project6/v2/main.c
+++ project6/v2/main.c
@@ -8,7 +8,7 @@
 
 typedef struct Instruction
 {
-    char *instruction;
+    char *name;
     char *binary;
 } Instruction;
 
@@ -273,7 +273,9 @@ char *append_char(char *str, char c)
 
 bool is_line_label(char *line)
 {
-    return (line[0] == '(') && (line[strlen(line) - 1] == ')');
+    return strlen(line) >= 2 &&
+        line[0] == '(' &&
+        line[strlen(line) - 1] == ')';
 }
 
 void run_first_pass(LineList *line_list, SymbolList *symbol_list)
@@ -298,6 +300,11 @@ void run_first_pass(LineList *line_list, SymbolList *symbol_list)
     }
 }
 
+bool is_line_A_instruction(char *line)
+{
+    return strlen(line) >= 2 && line[0] == '@';
+}
+
 void run_second_pass(LineList *line_list, SymbolList *symbol_list)
 {
     short value = 16;
@@ -305,9 +312,9 @@ void run_second_pass(LineList *line_list, SymbolList *symbol_list)
     {
         char *text = line_list->line->text;
         short number = line_list->line->number;
-        bool is_line_variable = text[0] == '@';
+        bool is_symbol = is_line_A_instruction(text) && !isdigit(text[1]);
 
-        if(is_line_variable)
+        if(is_symbol)
         {
             char *trimmed = ++text;
 
@@ -320,12 +327,75 @@ void run_second_pass(LineList *line_list, SymbolList *symbol_list)
     }
 }
 
-char *parse(LineList *list)
+void number_to_bits(char *bits, short value)
 {
-    //TODO Implement
-    for(LineList *i = list; i; i = i->next)
+    int i = strlen(bits) - 1; // -1 because MSB defines instruction type
+    while(i) {
+        bits[i] = (value % 2) + '0';
+        value = value / 2;
+        i--;
+    }
+}
+
+Instruction find_instruction_by_name(Instruction instructions[], size_t length, char *name)
+{
+    for(int i = 0; i < length; i++) {
+        if(strcmp(name, instructions[i].name) == 0) {
+            return instructions[i];
+        }
+    }
+}
+
+void *parse(char *buffer, LineList *line_list)
+{
+    for(;line_list; line_list = line_list->next)
     {
-        printf("looping %d\n", i->line->number);
+        char *line = line_list->line->text;
+        char *destination = "null";
+        char *computation = "";
+        char *jump = "null";
+
+        if(is_line_A_instruction(line))
+        {
+            bool is_symbol = is_line_A_instruction(line) && !isdigit(line[1]);
+            if(is_symbol)
+            {
+                // TODO find symbol from symbol list and get its value into binary
+            } else 
+            {
+                short value = atoi(++line);
+                char bits[17] = "0000000000000000";
+                number_to_bits(bits, value);
+                printf("bits: %s = %d\n", bits, value);
+                // TODO append bits to buffer
+            }
+        } else
+        {
+            char bits[17] = "1000000000000000";
+            char *destination = strchr(line, '=');
+
+            if(destination)
+            {
+                size_t length = destination - line;
+                char substring[20];
+
+                strncpy(substring, line, length);
+                substring[length] = '\0';
+                destination = substring;
+
+                Instruction dest_instruction = find_instruction_by_name(
+                        dest_instructions,
+                        sizeof(dest_instructions),
+                        destination);
+                
+                bits[10] = dest_instruction.binary[0];
+                bits[11] = dest_instruction.binary[1];
+                bits[12] = dest_instruction.binary[2];
+                printf("bits: %s %s was added\n", bits, dest_instruction.binary);
+            }
+        }
+        
+        //printf("looping %d\n", line_list->line->number);
     }
 }
 
@@ -358,18 +428,21 @@ int main(void)
             line = alloc_string();
         }
     }
-    //print_line_list(line_list);
 
     SymbolList *symbol_list = create_symbol_list();
     initialize_default_symbols(symbol_list);
 
     run_first_pass(line_list, symbol_list);
     run_second_pass(line_list, symbol_list);
-    //print_symbol_list(symbol_list);
-    print_line_list(line_list);
+
+    char *parse_buffer = alloc_string(); 
+    parse(parse_buffer, line_list);
+
+    puts("Testing");
 
     free_line_list(line_list);
     free_symbol_list(symbol_list);
+    free(parse_buffer);
     return 0;
 }
 
